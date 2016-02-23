bca704b0dc2a0249039356c943fe8e88039115fb
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2016-02-23 02:57:31 +0000

    Add basic statistics of orders and costumers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'stats', to: "stats#index" #/stats
   resources :orders, only: [:index] #/orders
   resources :tables, except: [:new, :edit] do
       resources :orders, only: [:create] do #/tables/:id/orders
